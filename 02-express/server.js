96a0077c39664c5660322ddafa1d820833818fe8
Robin Karlsson <robinkarlsson2001@gmail.com>
2022-02-02 16:15:18 +0100

    Added lodash and finished the workshop

--- 02-express/server.js
+++ 02-express/server.js
@@ -7,7 +7,8 @@ EXPRESS
 
 const express = require('express');
 const app = express();
-
+const fs = require('fs');
+const lodash = require('lodash')
 
 
 
@@ -50,7 +51,23 @@ app.get('/now', (req, res) => {
     res.send(`<h2>The current time is ${new Date}<h2>`);
 })
 
+// Respond with a random oneliner joke
+app.get('/jokes', (req, res) => {
+	// 1. Somehow read the JSON-contents of data/oneliners.json
+    let rawdata = fs.readFileSync(__dirname + '/data/oneliners.json');
+    let jokes = JSON.parse(rawdata);
+    
+	// 2. Get a random item from the array
+    shuffledJokes = lodash.shuffle(jokes)
+    randomJoke = shuffledJokes[0];
+
+	// 3. Respond with the item (`res.send(item)`)
+    res.send(randomJoke);
+});
+
 app.use(express.static('pages'));
+
+
 
 app.listen(3000,  () => {
     console.log("Server started sucsessfully");
