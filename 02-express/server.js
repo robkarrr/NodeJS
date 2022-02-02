59576ed8b5820c4677f7f9e67991f4c6176baff9
Robin Karlsson <robinkarlsson2001@gmail.com>
2022-02-02 13:37:44 +0100

    Connect to multiple url's

--- 02-express/server.js
+++ 02-express/server.js
@@ -9,12 +9,20 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    console.log("Someone requested my root");
+    console.log(req.method, req.url);
     
-    res.send("Hello World! HELLO IM GROOT!")
+    res.send("Hello from ROOT");
 })
 
+app.get('/nom', (req, res) => {
+    console.log(req.method, req.url);
+    
+    res.send("Lorem ipsum Lorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsum");
+})
 
+app.get('/api/nom', (res, req) => {
+    res.send({msg: 'nom-nom-nom-nom'});
+})
 
 app.listen(3000,  () => {
     console.log("Server started sucsessfully");
