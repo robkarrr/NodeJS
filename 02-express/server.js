18d0c58d57129a6f8eb0f9a0456f083ed06140e8
Robin Karlsson <robinkarlsson2001@gmail.com>
2022-02-02 12:00:56 +0100

    added res req and a response

--- 02-express/server.js
+++ 02-express/server.js
@@ -8,6 +8,14 @@ EXPRESS
 const express = require('express');
 const app = express();
 
+app.get('/', (req, res) => {
+    console.log("Someone requested my root");
+    
+    res.send("Hello World! HELLO IM GROOT!")
+})
+
+
+
 app.listen(3000,  () => {
     console.log("Server started sucsessfully");
-})
+});
