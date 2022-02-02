d6390e8f7fe108caaa4fb53de98e9bfb1b52c3bc
Robin Karlsson <robinkarlsson2001@gmail.com>
2022-02-02 14:31:39 +0100

    Send files instead of only text

--- 02-express/server.js
+++ 02-express/server.js
@@ -8,6 +8,9 @@ EXPRESS
 const express = require('express');
 const app = express();
 
+
+
+/*
 app.get('/', (req, res) => {
     console.log(req.method, req.url);
     
@@ -23,7 +26,26 @@ app.get('/nom', (req, res) => {
 app.get('/api/nom', (res, req) => {
     res.send({msg: 'nom-nom-nom-nom'});
 })
+*/
+
 
 app.listen(3000,  () => {
     console.log("Server started sucsessfully");
+});
+
+
+
+app.get('/about', (req, res) =>  {
+    res.sendFile(__dirname + '/pages/about.html');
+
+});
+
+app.get('/index', (req, res) =>  {
+    res.sendFile(__dirname + '/pages/index.html');
+    
+});
+
+app.get('/nom', (req, res) =>  {
+    res.sendFile(__dirname + '/pages/nom.html');
+    
 });
